f96d9f2d050b7cf28661ac778014a068c6129dfb
Jeremy Tsang <jeremy@jeremytsang.com>
2020-05-24 17:36:05 -0700

    Get assignment 05 working

--- site/app.js
+++ site/app.js
@@ -1,11 +1,94 @@
-var http = require('http')  // Import statement. Imports 'http' module.
-let port = 3000;
+// ----------------------------------------------------------------------------
+// ***** Setup code *****
 
-// .createServer takes function expression as eventh listener for when
-// an event comes in
-http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello world!');
-}).listen(port);
+let express = require('express');
 
-console.log(`Server started on localhost:${port}; press Ctrl-C to terminate....`);
+let app = express();
+
+let handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+
+let bodyParser = require('body-parser');
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3737);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/static'));
+// ----------------------------------------------------------------------------
+// ***** Routes *****
+
+/**
+ * Creates storage for request parameters.
+ */
+const getRequestParams = (req, res, next) => {
+  res.locals.queryData = makeNameValueArray(req.query);
+  res.locals.bodyData = makeNameValueArray(req.body);
+  next();
+};
+
+app.use(getRequestParams);
+
+// -------------------------------------
+// ***** GET request route *****
+app.get('/', (req, res) => {
+  let context = {
+    'queryParams': res.locals.queryData,
+  };
+  res.render('home-get', context);
+});
+
+// -------------------------------------
+// ***** POST request route *****
+app.post('/', (req, res) => {
+  let context = {
+    'queryParams': res.locals.queryData,
+    'bodyParams': res.locals.bodyData,
+  };
+  res.render('home-post', context);
+});
+
+// ----------------------------------------------------------------------------
+// ***** Error handlers *****
+app.use(function(req, res) {
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(error, req, res, next) {
+  console.error(error.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+// ----------------------------------------------------------------------------
+// ***** Event listener to listen for requests *****
+
+// process.env.HOSTNAME was taken from Greg Healy Video
+// https://www.youtube.com/watch?v=sw6rcXZiRos&feature=youtu.be
+
+app.listen(app.get('port'), function() {
+  console.log(
+    'Express started on http://'
+        + `${process.env.HOSTNAME}:${app.get('port')}`
+        + '; press Ctrl-C to terminate.'
+  );
+});
+
+// ----------------------------------------------------------------------------
+// ***** Helper functions *****
+/**
+ * Convert the property name/value pairs into an array of size 2 objects.
+ * @param {object} object - object to convert.
+ * @return {array} - Array of objects (name/value pairs of the argument).
+ */
+function makeNameValueArray(object) {
+  let params = [];
+
+  for (let name in object) {
+    params.push({'name': name, 'value': object[name]});
+  }
+
+  return params;
+}
